265146a06dfe28b6219d91c6e8fe6c7137eb5c69
MosqueraP <mcplinio.15@gmail.com>
2023-03-11 01:07:21 -0500

    Agregando proyecto

---
+++ operaciones.py
@@ -0,0 +1,16 @@
+# Solicitamos al usuarios ingresar unos numeros con el 
+# cual realizaremos algunas operaciones basicas en python
+num1 = int(input('Ingresa un numero": '))
+num2 = int(input('Ingresa un numero: '))
+
+# Operaciones basicas de matematicas
+suma = num1 + num2
+resta = num1 - num2
+mult = num1 * num2
+div = num1 // num2
+
+
+print('La suma es ', suma)
+print('La resta es', resta)
+print('La mult es', mult)
+print('La divicion es', div)
